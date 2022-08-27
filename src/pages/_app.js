91f20c4b37af3e1cc6448ddea6d233f9964a41b3
AzureKn1ght <philsonnah@msn.com>
2022-08-28 00:26:49 +0800

    Updated Head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Vie</title>
+        <title>Lov3struck</title>
         <link rel="icon" href="/img/favicon.ico" />
       </Head>
       <Cursor />
